5637987652578d4d8722360a9db76963357491ce
Aditia A. Pratama <aditia.ap@gmail.com>
2018-08-10 02:31:26 +0700

    fix error with index tupple

--- c2o_ops.py
+++ c2o_ops.py
@@ -12,24 +12,29 @@ class c2oDiffuseConvert(bpy.types.Operator):
         objects = bpy.data.objects
         scenes = bpy.data.scenes
         scenes[0].render.engine = 'octane'
-        for obj in objects:
-            materials = [m for m in bpy.data.materials if m.use_nodes]
-            for m in materials:
-                nodes = m.node_tree.nodes
-                links = m.node_tree.links
-                diff_nodes = [n for n in nodes if n.type == 'BSDF_DIFFUSE']
-                for n in diff_nodes:
-                    n_loc_x = n.location.x - 250
-                    n_loc_y = n.location.y
-                    n_color = n.inputs[0].default_value
-                    n_link = n.outputs[0].links[0].to_socket
-                    shader_oct_diff = nodes.new('ShaderNodeOctDiffuseMat')
-                    shader_oct_diff.location = n_loc_x, n_loc_y
-                    shader_oct_diff.inputs[0].default_value = n_color
-                    links.new(
-                        shader_oct_diff.outputs[0],
-                        n_link
-                        )
+        materials = [m for m in bpy.data.materials if m.use_nodes]
+        for m in materials:
+            nodes = m.node_tree.nodes
+            links = m.node_tree.links
+            diff_nodes = [n for n in nodes if n.type == 'BSDF_DIFFUSE']
+            for n in diff_nodes:
+                n_loc_x = n.location.x - 250
+                n_loc_y = n.location.y
+                n_color = n.inputs[0].default_value
+                n_outputs = [o for o in n.outputs]
+                for o in n_outputs:
+                    n_links = [l for l in o.links]
+                    for l in n_links:
+                        n_socket = l.to_socket
+                #n_link = n.outputs[0].links[0].to_socket
+                shader_oct_diff = nodes.new('ShaderNodeOctDiffuseMat')
+                shader_oct_diff.location = n_loc_x, n_loc_y
+                shader_oct_diff.inputs[0].default_value = n_color
+                links.new(
+                    shader_oct_diff.outputs[0],
+                    n_socket
+                    )
+                nodes.remove(n)
 
         return {"FINISHED"}
 
