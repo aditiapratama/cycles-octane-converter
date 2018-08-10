864df25e28b2d96042c4bc1ba7839a919625c433
Aditia A. Pratama <aditia.ap@gmail.com>
2018-08-10 15:12:58 +0700

    add feature to convert float image, also now will convert all mix node to octane mix node

--- c2o_ops.py
+++ c2o_ops.py
@@ -43,6 +43,54 @@ def diffuse_convert(context):
                 )
             nodes.remove(n)
 
+def mixmat_convert(context):
+    materials = [m for m in bpy.data.materials if m.use_nodes]
+    for m in materials:
+        nodes = m.node_tree.nodes
+        links = m.node_tree.links
+        mix_nodes = [n for n in nodes if n.type == 'MIX_SHADER']
+        for n in mix_nodes:
+            n_loc_x = n.location.x 
+            n_loc_y = n.location.y
+
+            shader_oct_mix = nodes.new('ShaderNodeOctMixMat')
+            shader_oct_mix.location = n_loc_x, n_loc_y
+
+            n_inputs = n.inputs[1]
+            i_links = [i for i in n_inputs.links]
+            for i in i_links:
+                from_socket_1 = i.from_socket
+
+            if n_inputs.is_linked:
+                links.new(
+                    shader_oct_mix.inputs[1],
+                    from_socket_1
+                )
+            n_inputs = n.inputs[2]
+            i_links = [i for i in n_inputs.links]
+            for i in i_links:
+                from_socket_2 = i.from_socket
+
+            if n_inputs.is_linked:
+                links.new(
+                    shader_oct_mix.inputs[2],
+                    from_socket_2
+                )
+
+            n_outputs = n.outputs
+            o_links = [o for o in n_outputs]
+            for o in o_links:
+                o_links = o.links
+                for ol in o_links:
+                    to_socket = ol.to_socket
+            
+            links.new(
+                shader_oct_mix.outputs[0],
+                to_socket
+                )
+            nodes.remove(n)
+
+
 def image_convert(context):
     materials = [m for m in bpy.data.materials if m.use_nodes]
     for m in materials:
@@ -70,7 +118,35 @@ def image_convert(context):
                 to_socket
                 )
             nodes.remove(n)
+        
+def floatimg_convert(context):
+    materials = [m for m in bpy.data.materials if m.use_nodes]
+    for m in materials:
+        nodes = m.node_tree.nodes
+        links = m.node_tree.links
+        floatimg_nodes = [n for n in nodes if n.type == 'TEX_IMAGE'\
+                    and n.color_space == 'NONE']
+        for n in floatimg_nodes:
+            n_loc_x = n.location.x 
+            n_loc_y = n.location.y
+            n_image= n.image
 
+            shader_oct_floatimg = nodes.new('ShaderNodeOctFloatImageTex')
+            shader_oct_floatimg.location = n_loc_x, n_loc_y
+            shader_oct_floatimg.image = n_image
+
+            n_outputs = n.outputs
+            o_links = [o for o in n_outputs]
+            for o in o_links:
+                o_links = o.links
+                for ol in o_links:
+                    to_socket = ol.to_socket
+            
+            links.new(
+                shader_oct_floatimg.outputs[0],
+                to_socket
+                )
+            nodes.remove(n)
 
 def glossy_convert(context):
     materials = [m for m in bpy.data.materials if m.use_nodes]
@@ -101,6 +177,41 @@ def glossy_convert(context):
             )
             nodes.remove(n)
 
+
+def principled_convert(context):
+    materials = [m for m in bpy.data.materials if m.use_nodes]
+    for m in materials:
+        nodes = m.node_tree.nodes
+        links = m.node_tree.links
+
+        principled_nodes = [n for n in nodes if n.type == 'BSDF_PRINCIPLED']
+        for n in principled_nodes:
+            n_loc_x = n.location.x 
+            n_loc_y = n.location.y - 225
+            n_color = n.inputs[0].default_value
+            n_specular = n.inputs['Specular'].default_value
+            n_roughness = n.inputs['Roughness'].default_value
+            n_ior = n.inputs['IOR'].default_value
+            
+            shader_oct_glossy = nodes.new('ShaderNodeOctGlossyMat')
+            shader_oct_glossy.location = n_loc_x, n_loc_y
+            shader_oct_glossy.inputs[0].default_value = n_color
+            shader_oct_glossy.inputs['Specular'].default_value = n_specular
+            shader_oct_glossy.inputs['Roughness'].default_value = n_roughness
+            shader_oct_glossy.inputs['Index'].default_value = n_ior
+
+            n_outputs = n.outputs
+            o_links = [o for o in n_outputs]
+            for o in o_links:
+                o_links = o.links
+                for ol in o_links:
+                    to_socket = ol.to_socket
+
+            links.new(
+                shader_oct_glossy.outputs[0],
+                to_socket
+            )
+            nodes.remove(n)
 
 def glass_convert(context):
     materials = [m for m in bpy.data.materials if m.use_nodes]
@@ -141,10 +252,14 @@ class c2oDiffuseConvert(bpy.types.Operator):
     def execute(self, context):
         scenes = bpy.data.scenes
         scenes[0].render.engine = 'octane'
+    
         diffuse_convert(self)
         image_convert(self)
+        floatimg_convert(self)
         glossy_convert(self)
+        principled_convert(self)
         glass_convert(self)
+        mixmat_convert(self)
 
         return {"FINISHED"}
 
