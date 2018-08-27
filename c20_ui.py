36e42763bda6506427f0665c77dc9141242902ed
Aditia A. Pratama <aditia.ap@gmail.com>
2018-08-27 16:22:32 +0700

    Revert "new feature for apply convert to select only objects"
    This reverts commit 1d0884e681b2bbc77111221c7c386c1439acef44.

--- c20_ui.py
+++ c20_ui.py
@@ -12,15 +12,23 @@ class c2oPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         box = layout.box()
-        c2oSettings = bpy.context.scene.c2oSettings
         col = box.column(align=True)
-        col.prop(
-            c2oSettings,
-            "mode_select",
-            text = ''
-        )
         col.operator(
-            "c2oconvert.all",
+            "c2oconvert.diffuse",
             text="Convert",
             icon="ALIGN"
         )
+
+
+def register():
+    from bpy.utils import register_class
+
+    register_class(c2oPanel)
+
+def unregister():
+    from bpy.utils import unregister_class
+
+    unregister_class(c2oPanel)
+
+if __name__ == "__main__":
+    register()
